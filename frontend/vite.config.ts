422afe5e711e02791e1a3640ec30a0ee82e553fc
Jantulu <Alex.Hartlaub@gmail.com>
2025-12-30 15:20:35 -0600

    update vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts: "office.home.arpa",
     proxy: {
       '/api': {
         target: 'http://backend:8000',
